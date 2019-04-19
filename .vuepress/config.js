7d1cdbae4bd1113170d68975996da7678d4f1025
Mr.Blue <silverhugh.77@gmail.com>
2019-04-19 13:10:50 +0800

    :sparkles: Add google analytics.

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,5 +2,13 @@ module.exports = {
     title: 'Mr.Blue\'s Blog',
     markdown: {
         linkify: true
-    }
+    },
+    plugins: [
+        [
+            '@vuepress/google-analytics',
+            {
+                'ga': 'UA-138694157-1'
+            }
+        ]
+    ]
 }
