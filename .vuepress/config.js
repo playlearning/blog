c7c173643b9df4bde08495b4de98dc00e31bef32
Mr.Blue <silverhugh.77@gmail.com>
2019-04-23 14:54:48 +0800

    update icon.

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,5 +1,8 @@
 module.exports = {
     title: 'Mr.Blue\'s Blog',
+    head: [
+        ['link', { rel: 'icon', href: '/shuttlecock-transparent-small.png' }]
+    ],
     markdown: {
         linkify: true
     },
