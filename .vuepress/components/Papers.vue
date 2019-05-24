a98a28cffe7780c435b53b0d41cd687184dc1b3f
Mr.Blue <silverhugh.77@gmail.com>
2019-05-24 23:40:40 +0800

    :beers: update.

--- .vuepress/components/Papers.vue
+++ .vuepress/components/Papers.vue
@@ -5,66 +5,72 @@
         <a class="has-text-black" :href="'#' + topic_id">Topic: {{ paper_list.topic }}</a>
       </p>
       <p>
-        <span class="button is-small" v-on:click="toggle_verbose">
-          <span v-if="is_verbose">Hide</span>
-          <span v-else>Show</span>
+        <span class="button is-light is-small" v-on:click="toggle_verbose">
+          <span v-if="is_verbose" class="verbose-text">Hide</span>
+          <span v-else class="verbose-text">Show</span>
           Detail
         </span>
-        <a class="button is-small btn-link" :href="src" target="_blank">YAML</a>
-        <a class="button is-small btn-link" href="https://github.com/playlearning/blog/tree/master/.vuepress/public/yaml/papers" target="_blank">Download Papers in List</a>
+        <a class="button is-small is-light btn-link" :href="src" target="_blank">
+            YAML &nbsp;
+            <i class="fas fa-cogs"></i>
+        </a>
+        <a class="button is-small is-light btn-link" href="https://github.com/playlearning/blog/tree/master/.vuepress/public/yaml/papers" target="_blank">
+            Download Papers in List &nbsp;
+            <i class="fas fa-download"></i>
+        </a>
       </p>
+      <p class="title is-5" v-if="Object.keys(tasks).length">Tasks</p>
+      <div class="field is-grouped is-grouped-multiline">
+        <div class="control" v-for="(c, t) in tasks">
+          <div class="tags has-addons">
+            <span class="tag">{{ t }}</span>
+            <span class="tag has-background-grey-lighter">{{ c }}</span>
+          </div>
+        </div>
+      </div>
       <p class="title is-5">Resources</p>
       <ol>
         <li v-for="(p, index) in papers">
-          <!--<span class="index">-->
-            <!--{{index+1}}.-->
-          <!--</span>-->
-          <span v-bind:class="{ stress: p.stress }">
-            <span v-if="p.date">[{{ new Date(String(p.date)).getFullYear() }}]</span>
-            <a v-if="p.link" :href="p.link" target="_blank">{{ p.title }}</a>
-            <span v-else>{{ p.title }}</span>
-            <a v-if="p.pdf" :href="p.pdf" target="_blank"><i class="far fa-file-pdf is-size-7 has-text-black"></i></a>
-          </span>
-          <span v-if="p.code">
-              <a :href="p.code" target="_blank"><i class="far fa-file-code has-text-black is-size-7"></i></a>
-          </span>
-          <span v-if="p.proceeding" class="has-text-grey is-size-7 is-italic has-text-weight-semibold">{{ p.proceeding }}</span>
-          <div v-if="is_verbose" class="has-background-light">
-            <div v-if="p.questions">
-              <p class="title is-6">Questions:</p>
-              <ol>
-                <li v-for="(q, index) in p.questions">
-                  <!--<strong>{{ index+1 }}.</strong>-->
-                  {{ q }}
-                </li>
-              </ol>
+            <span v-bind:class="{ stress: p.stress }">
+              <span v-if="p.date">[{{ new Date(String(p.date)).getFullYear() }}]</span>
+              <a v-if="p.link" :href="p.link" target="_blank">{{ p.title }}</a>
+              <span v-else>{{ p.title }}</span>
+              <a v-if="p.pdf" :href="p.pdf" target="_blank"><i class="far fa-file-pdf is-size-7 has-text-black"></i></a>
+            </span>
+            <span v-if="p.code">
+                <a :href="p.code" target="_blank"><i class="far fa-file-code has-text-black is-size-7"></i></a>
+            </span>
+            <span v-if="p.proceeding" class="has-text-grey is-size-7 is-italic has-text-weight-semibold">{{ p.proceeding }}</span>
+          <div v-if="is_verbose && has_detail(p)" class="detail">
+            <div>
+              <div v-if="p.questions" class="media">
+                <div class="media-left">
+                  <i class="far fa-question-circle is-size-5"></i>
+                </div>
+                <div class="media-content">
+                  <ol>
+                    <li v-for="(q, index) in p.questions">
+                      {{ q }}
+                    </li>
+                  </ol>
+                </div>
+              </div>
+              <div v-if="p.tasks" class="media">
+                <div class="media-left">
+                  <i class="fas fa-crosshairs is-size-5"></i>
+                </div>
+                <div class="media-content">
+                  <ol>
+                    <li v-for="(t, index) in p.tasks">
+                      {{ t.name }}
+                    </li>
+                  </ol>
+                </div>
+              </div>
             </div>
           </div>
         </li>
       </ol>
-      <div v-if="paper_list.tasks && paper_list.tasks.length">
-        <p>
-          <strong><span class="title is-5">Tasks</span></strong>
-        </p>
-        <ol>
-          <li v-for="(t, index) in paper_list.tasks">
-            <!--<span class="index">-->
-              <!--{{index+1}}.-->
-            <!--</span>-->
-            <span>{{ t.name }}</span>
-            <div v-if="t.datasets && t.datasets.length" >
-              <ul>
-                <li v-for="(d, index) in t.datasets">
-                  <a :href="d.link">
-                    <span v-if="d.abbr">{{ d.abbr}}</span>
-                    <span v-else>{{ d.name }}</span>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </li>
-        </ol>
-      </div>
       <div v-if="references.length">
         <p class="title is-5">References</p>
         <ol class="ref-list">
@@ -124,11 +130,31 @@ export default {
       } else {
         return ''
       }
+    },
+    tasks: function() {
+      var res = {}
+      if (this.paper_list.papers) {
+        for (const p of this.paper_list.papers) {
+          if (p.tasks) {
+            for (const t of p.tasks) {
+              if (t.name in res) {
+                res[t.name] += 1
+              } else {
+                res[t.name] = 1
+              }
+            }
+          }
+        }
+      }
+      return res
     }
   },
   methods: {
     toggle_verbose: function() {
       this.is_verbose = !this.is_verbose
+    },
+    has_detail: function(p) {
+      return !!(p.questions || p.tasks);
     }
   }
 }
@@ -154,13 +180,14 @@ export default {
   margin-left: 0.5em;
 }
 .detail {
-  padding: 0em 2em;
-  background: #f8f8f8;
+  margin: 0 2.5em;
+  padding: 0.5em;
   border-radius: 3px;
 }
 ol {
   list-style-type: none !important;
   counter-reset: elementcounter;
+  margin-top: 0;
 }
 ol > li:before {
   display: inline-block;
@@ -176,5 +203,14 @@ ol > li:before {
 }
 .topic_head {
   margin-top: 1em;
+}
+.media-content ol {
+  margin-left: 0;
+}
+.media-content {
+  border-left: 1px solid #eee;
+}
+.verbose-text {
+    width: 2.8em;
 }
 </style>
--- snippet/ml-base/README.md
+++ snippet/ml-base/README.md
@@ -1,3 +1,7 @@
 # Machine Learning Basics
 
+## Topics
+
 <Papers src="/yaml/papers/ml_base.yaml"></Papers>
+
+<Papers src="/yaml/papers/nlp_basics.yaml"></Papers>
